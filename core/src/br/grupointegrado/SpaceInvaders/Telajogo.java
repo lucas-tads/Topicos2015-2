000892c20986fc535af7c22b3f4662c4abd7a8f7
Lucas Teodoro Dos Santos <lucas_teodoro775@hotmail.com>
2015-08-10 21:04:35 -0300

    commit aula 10/08, deslocamento do ator e troca de imagens

--- core/src/br/grupointegrado/SpaceInvaders/Telajogo.java
+++ core/src/br/grupointegrado/SpaceInvaders/Telajogo.java
@@ -1,17 +1,20 @@
 package br.grupointegrado.SpaceInvaders;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.badlogic.gdx.utils.viewport.FillViewport;
-import com.sun.org.apache.xpath.internal.operations.Or;
 
-import java.awt.Color;
-import java.awt.image.ColorConvertOp;
+import sun.rmi.runtime.Log;
 
 /**
  * Created by lucas on 03/08/2015.
@@ -23,6 +26,12 @@ public class Telajogo extends Telabase{
     private Stage palco;
     private BitmapFont fonte;
     private Label lbPontuacao;
+    private Image jogador;
+    private Texture texturajogador;
+    private Texture texturajogadorDireita;
+    private Texture texturajogadorEsquerda;
+    private boolean indodireita;
+    private boolean indoesquerda;
 
     /**
      * Construtor padão da tela de jogo,
@@ -43,6 +52,19 @@ public class Telajogo extends Telabase{
 
         initFonte();
         initInformacoes();
+        initJogador();
+    }
+
+    private void initJogador() {
+        texturajogador = new Texture("sprites/player.png");
+        texturajogadorDireita = new Texture("sprites/player-right.png");
+        texturajogadorEsquerda = new Texture("sprites/player-left.png");
+
+        jogador = new Image(texturajogador);
+        float x = camera.viewportWidth/2 - jogador.getWidth() / 2;
+        float y = 15;
+        jogador.setPosition(x, y);
+        palco.addActor(jogador);
     }
 
     private void initInformacoes() {
@@ -67,9 +89,58 @@ public class Telajogo extends Telabase{
         Gdx.gl.glClearColor(.25f, .15f, .25f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+
         lbPontuacao.setPosition(10, camera.viewportHeight - 20);
+        capturaTeclas();
+        atualizarJogador(delta);
+
         palco.act(delta);
         palco.draw();
+    }
+
+    private void atualizarJogador(Float delta) {
+        float velocidade =200; //velocidade de movimento do jogador
+
+        if (indodireita){
+            if (jogador.getX() < camera.viewportWidth - jogador.getWidth()) {
+                float x = jogador.getX() + velocidade * delta;
+                float y = jogador.getY();
+                jogador.setPosition(x, y);
+            }
+        }
+        else if(indoesquerda){
+            if (jogador.getX() > 0){
+                float x = jogador.getX() - velocidade * delta;
+                float y = jogador.getY();
+                jogador.setPosition(x, y);
+            }
+        }
+
+        if(indodireita){
+            //trocar imagem direita
+            jogador.setDrawable(new SpriteDrawable(new Sprite(texturajogadorDireita)));
+        }else if (indoesquerda){
+            //trocar imagem esquerda
+            jogador.setDrawable(new SpriteDrawable(new Sprite(texturajogadorEsquerda)));
+        }else{
+            //trocar imagem do centro
+            jogador.setDrawable(new SpriteDrawable(new Sprite(texturajogador)));
+        }
+    }
+
+
+    /**
+     * verifica se as teclas estão pressionadas
+     */
+    private void capturaTeclas() {
+        indodireita = false;
+        indoesquerda = false;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            indoesquerda = true;
+        }else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            indodireita = true;
+        }
     }
 
     /**
@@ -107,5 +178,8 @@ public class Telajogo extends Telabase{
         batch.dispose();
         palco.dispose();
         fonte.dispose();
+        texturajogador.dispose();
+        texturajogadorEsquerda.dispose();
+        texturajogadorDireita.dispose();
     }
 }
